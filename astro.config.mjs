b0d661c3a57fa000fdea0c3465ea370f91c1fc06
Joe Strouth <joestrouth1@gmail.com>
2025-05-20 07:37:47 -0400

    initial reproduction

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    env: {
+        schema: {
+            JAZZ_CLOUD_KEY: envField.string({
+                access: 'public',
+                context: 'client'
+            })
+        }
+    }
+});
